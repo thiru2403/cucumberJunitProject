b5cb25968bf43b3c9e1e9746eb174fa7f131737e
unknown <thiruselvam20000@gmail.com>
2022-07-11 10:11:35 +0530

    space

--- src/test/java/com/vcentry/lab/runner/TestRunner.java
+++ src/test/java/com/vcentry/lab/runner/TestRunner.java
@@ -11,7 +11,7 @@ features="src/test/java/com/vcentry/lab/features",
 glue ="com.vcentry.lab.stepDefinition",
 plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
  monochrome= true,// readable console
-dryRun=false,//dry run used to check features file steps were implemented in TestSteps
+dryRun=false, //dry run used to check features file steps were implemented in TestSteps
 
 tags ="@validlogin or @sanity"
 )
